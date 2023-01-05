53f4560f5d1596e34f4c083281a33cee7543f327
Samuel O. Asare <asare11samuel@gmail.com>
2023-01-05 12:58:56 +0000

    feat: dynamic urls in templates

--- meetups/urls.py
+++ meetups/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("meetups/", views.index),
-    path("meetups/<slug:meetup_slug>/", views.details)
+    path("meetups/", views.index, name="all-meetups"),
+    path("meetups/<slug:meetup_slug>/", views.details, name="meetup-details")
 ]
