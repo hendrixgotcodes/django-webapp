0c467cc8d51c623f81ae365a0944caf760a79e19
Samuel O. Asare <asare11samuel@gmail.com>
2023-01-05 10:12:42 +0000

    feat: styled meetups index page

--- meetups/views.py
+++ meetups/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     meetups = [
-        {"title": 'A First Meetup'},
-        {"title": 'A Second Meetup'}
+        {"title": 'A First Meetup', 'location': 'New york', 'slug': 'a-first-meetup'},
+        {"title": 'A Second Meetup', 'location': 'New jersey', 'slug': 'a-second-meetup'}
     ]
     return render(request, "meetups/index.html",{'meetups':meetups, 'show_meetups': True})
