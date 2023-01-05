0ce78e4a00662955bfcc36a1733649a03723210a
Samuel O. Asare <asare11samuel@gmail.com>
2023-01-05 10:35:16 +0000

    created details page

--- meetups/views.py
+++ meetups/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
@@ -8,3 +8,16 @@ def index(request):
         {"title": 'A Second Meetup', 'location': 'New jersey', 'slug': 'a-second-meetup'}
     ]
     return render(request, "meetups/index.html",{'meetups':meetups, 'show_meetups': True})
+
+def details(request):
+    selected_meetup = {
+        'title': 'A First Meetup',
+        'location': 'New york',
+        'slug': 'a-first-meetup',
+        'description': 'this is the selected meetup'
+    }
+    
+    return render(request, "meetups/details.html", {
+        'meetup_title': selected_meetup['title'],
+        'meetup_description': selected_meetup['description'],
+    })
