32185013325beb32f52d40ab9572b452c42a5f23
Samuel O. Asare <asare11samuel@gmail.com>
2023-01-05 00:34:53 +0000

    feat: basic template language

--- meetups/views.py
+++ meetups/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "meetups/index.html")
+    meetups = [
+        {"title": 'A First Meetup'},
+        {"title": 'A Second Meetup'}
+    ]
+    return render(request, "meetups/index.html",{'meetups':meetups, 'show_meetups': True})
